aa7259a3d7eab5afafdac2c65db2023eb9233601
kirkeb <kirkeb1030@yahoo.com>
2023-07-27 16:40:12 -0400

    End of Sevtin 6 - Refactor and Repository

---
+++ HotelListing.API/Models/Country/UpdateCountryDto.cs
@@ -0,0 +1,9 @@
+﻿namespace HotelListing.API.Models.Country
+{
+    public class UpdateCountryDto : BaseCountryDto { 
+    
+        public int Id { get; set; }
+
+    }
+}
+
---
+++ HotelListing.API/Repository/GenericRepository.cs
@@ -0,0 +1,57 @@
+﻿using HotelListing.API.Contracts;
+using HotelListing.API.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelListing.API.Repository
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly HotelListingDbContext context;
+
+        public GenericRepository(HotelListingDbContext _context)
+        {
+            this.context = _context;
+        }
+
+        public async Task<T> AddAsync(T entity)
+        {
+            await context.AddAsync(entity);
+            await context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var entity = await GetAsync(id);
+            context.Remove(entity);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task<bool> Exists(int id)
+        {
+            var entity = await GetAsync(id); 
+            return entity != null;
+        }
+
+        public async Task<List<T>> GetAllAsync()
+        {
+            return await context.Set<T>().ToListAsync();
+        }
+
+        public async Task<T> GetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return null;
+            }
+
+            return await context.Set<T>().FindAsync(id);
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            context.Update(entity);
+            await context.SaveChangesAsync();
+        }
+    }
+}
